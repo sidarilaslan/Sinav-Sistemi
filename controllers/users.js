67df4af1dc018c59fca16cc3ed8a7c1fa2e1b5dd
sidar <ilaslansidar@gmail.com>
2022-05-03 18:43:27 +0300

    feat: getUser route eklendi

--- controllers/users.js
+++ controllers/users.js
@@ -1,12 +1,28 @@
 const { connectDB } = require('../loaders/db');
 
-
 let getUsers = connectDB().then(async result => {
-    return (await result.query('SELECT * FROM tblUser')).recordset;
+    return (await (result.query('SELECT * FROM tblUser'))).recordset;
 
 });
 
+async function getUser(userID) {
+    let x = await connectDB();
+    return (await x.query('SELECT * FROM tblUser where userID = ' + userID)).recordset;
+
+}
+
+// let getUser = (userID) => {
+
+//     console.log(userID);
+//     connectDB().then(async result => {
+//         return (await result.input('input_parameter', sql.Int, userID).query('SELECT * FROM tblUser where userID = @input_parameter')).recordset;
+
+//     });
+// }
+
 
 module.exports = {
-    getUsers
+    getUsers,
+    getUser
+
 }
--- routers/users.js
+++ routers/users.js
@@ -3,16 +3,22 @@ const { userController } = require('../controllers/index');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-
     userController.getUsers.then(result => {
         res.send(result);
+        res.end();
     });
 
 
 
 });
 
+router.get('/:id', (req, res) => {
 
+    userController.getUser(req.params.id).then(result => {
+        res.send(result);
+        res.end();
+    });
+});
 
 
 module.exports = router;
